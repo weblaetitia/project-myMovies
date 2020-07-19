c5cb6bcef8a092f2ec8c6391701907e89284f23d
Laetitia Langlois <weblaetitia@hotmail.com>
2020-07-19 17:06:47 +0200

    merge navigation and wishlist in app.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,9 +1,12 @@
 import React, {useState, useEffect} from 'react'
 import './App.css'
-import { Navigation } from './components/Nav'
 import { Movies } from './components/Movie'
 
 import { Container, Row, ListGroupItem, Media } from 'reactstrap';
+import { Nav, NavItem, NavLink } from 'reactstrap';
+import {Button, UncontrolledPopover, PopoverHeader, PopoverBody, ListGroup} from 'reactstrap'
+
+
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
@@ -87,7 +90,27 @@ function App() {
 
    <div>
      <Container className="mb-4">
-      <Navigation myWishList={myWishList} numberOfMovies={numberWish} />
+     <div className="navbar">
+        <Nav>
+          <NavItem>
+          <NavLink href="#"><img src="../img/logo.png" width="42" height="36" alt="" loading="lazy" /></NavLink>
+          </NavItem>
+          <NavItem>
+          <NavLink style={{color: 'white'}} href="#">Last release</NavLink>
+          </NavItem>
+        </Nav>
+
+        <Button href="#" className="ml-auto" id="PopoverLegacy" type="button">{numberWish} films</Button>
+
+        <UncontrolledPopover trigger="legacy" placement="bottom" target="PopoverLegacy">
+        <PopoverHeader>Ma wishlist</PopoverHeader>
+        <PopoverBody>
+        <ListGroup className='list-group-flush'>
+          {myWishList}
+        </ListGroup>
+        </PopoverBody>
+      </UncontrolledPopover>
+        </div>
      </Container>
     
     <Container>
